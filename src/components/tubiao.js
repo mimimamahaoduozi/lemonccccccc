7ce45c0c029f7bc469ad63c5e8030a4398c0bb66
“ <924183519@qq.com>
2018-09-10 22:18:12 +0800

    基本完成

--- src/components/tubiao.js
+++ src/components/tubiao.js
@@ -3,11 +3,10 @@ import { Chart, Tooltip, Axis, Bar } from 'viser-react';
 const scale = [{
     dataKey: 'sales',
     tickInterval: 1,
-    nice: false,
+    nice: true,
     min: 0,
 },{
-    dataKey: 'count',
-    max: 14,
+    dataKey: 'genre',
 }];
 class Tubiao1 extends Component{
     componentWillReceiveProps(){
@@ -17,8 +16,8 @@ class Tubiao1 extends Component{
         return (
             <div>
                 <Chart forceFit height={100}  data={this.props.data} scale={scale} padding={[ 0, 0, 0, 0]}>
-                    <Tooltip />
-                    <Bar position="genre*sold" color={'#0fa'} binWidth={0}/>
+                    <Tooltip inPlot={false}/>
+                    <Bar position="genre*sold" color={'#0a0'} />
                 </Chart>
             </div>
         )
